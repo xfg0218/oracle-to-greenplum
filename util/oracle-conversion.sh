0f819937784edb607be42bee84c30207a4c7667f
xiaoxu <1234@qq.com>
2019-05-13 15:35:29 +0800

    update file

--- util/oracle-conversion.sh
+++ util/oracle-conversion.sh
@@ -2,14 +2,18 @@
 
 # 链接字符串信息
 export NLS_LANG='SIMPLIFIED CHINESE_CHINA.ZHS16GBK'
-export DB_ORCL='newdaas/Fcdt201901NewDaas@192.168.205.30/orcl'
+export DB_ORCL='username/password@ip/orcl'
 
 
 #  c_gs_invite_baseinfo_xiaoxu 表的数据重命名
 
-echo -e "\t \n c_gs_invite_baseinfo_xiaoxu ...... \n"
+echo -e "\t \n updateinfo_xiaoxu 转换数据开始 ...... \n"
 
 time sqlplus -s $DB_ORCL<<EOF
+
+
+-- oracle   sql   conversion
+
 
 quit;
 
